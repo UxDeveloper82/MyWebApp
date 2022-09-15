6df3e9e42a590ece70d0945aedba206fee450544
Martin Fernandez <Electronicsmf@aol.com>
2022-09-15 07:21:22 -0400

    Adding Forsale Page

--- js/pratices.js
+++ js/pratices.js
@@ -108,3 +108,25 @@ const fetchData = async() => {
 }
 
 fetchData();
+
+const restaurant = {
+    name :"Classico Italiano",
+    location: "Via Angelo",
+    categories: ['Italian', 'Pizzeria','Vegetarian']
+
+}
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x , y, z] = arr;
+console.log(x,y,z);
+console.log(arr);
+
+const [first, second] = restaurant.categories;
+
+console.log(first, second);
+
+
